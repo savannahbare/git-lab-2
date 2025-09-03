2971480ed40913f1c3c099a16a98380aaea95fdd
Savannah <savannahbare99@gmail.com>
2025-09-03 18:03:59 -0400

    Done with git-lab-2

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Savannah Bare
+ *        Date: 9/3/25
+ * Description: Printing "Hello Git!!" to screen.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
